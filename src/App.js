ad30614683295a289e0cc05a2c9027b8e4d3cd79
Ashkan Kardan <ashkankardan14@gmail.com>
2020-12-24 02:22:44 +0300

    search component completed - adding deployed to github pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import React, { Component } from "react";
 import { CardList } from "./components/card-list/Card-list";
+import { Search } from "./components/search/Search";
 
 class App extends Component {
   constructor() {
@@ -8,8 +9,13 @@ class App extends Component {
 
     this.state = {
       contacts: [],
+      search: "",
     };
   }
+
+  handleChange = (e) => {
+    this.setState({ search: e.target.value });
+  };
 
   componentDidMount() {
     fetch("https://jsonplaceholder.typicode.com/users")
@@ -21,9 +27,16 @@ class App extends Component {
   }
 
   render() {
+    const { contacts, search } = this.state;
+    const filteredContacts = contacts.filter((contact) =>
+      contact.name.toLowerCase().includes(search.toLowerCase())
+    );
+
     return (
-      <div className="App">
-        <CardList contacts={this.state.contacts} />
+      <div className="App container">
+        <h1 className="heading">Contact Lookup</h1>
+        <Search placeholder="Search Contact" handleChange={this.handleChange} />
+        <CardList contacts={filteredContacts} />
       </div>
     );
   }
